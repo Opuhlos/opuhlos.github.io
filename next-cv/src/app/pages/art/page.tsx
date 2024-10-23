de0f845bb589194952243c794bc7096ca9a5aeb9
opuhlos-eternity <ericnguyen455@gmail.com>
2024-10-23 15:11:09 -0400

    Fixed footers

--- next-cv/src/app/pages/art/page.tsx
+++ next-cv/src/app/pages/art/page.tsx
@@ -1,9 +1,11 @@
 import ArtNav from "../../components/Navigations/ArtNav";
+import FixedFooter from "@/app/components/Footers/FixedFooter";
 
 export default function Home() {
   return (
     <div>
       <ArtNav/>
+      <FixedFooter/>
     </div>
   );
 }
--- next-cv/src/app/pages/projects/page.tsx
+++ next-cv/src/app/pages/projects/page.tsx
@@ -1,9 +1,11 @@
 import ProjectsNav from "../../components/Navigations/ProjectsNav";
+import FixedFooter from "@/app/components/Footers/FixedFooter";
 
 export default function Home() {
   return (
     <div>
       <ProjectsNav/>
+      <FixedFooter/>
     </div>
   );
 }
