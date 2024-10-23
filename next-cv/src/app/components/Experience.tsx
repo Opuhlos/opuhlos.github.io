737b85c0472747c7e450350478ee6f3d0cf14afe
Opuhlos <140357050+Opuhlos@users.noreply.github.com>
2024-10-22 22:46:49 -0400

    progress on experience component

---
+++ next-cv/src/app/components/Experience.tsx
@@ -0,0 +1,46 @@
+export default function Experience() {
+    return(
+        <section className="lea-section-container">
+        <div className="lea-top">
+            <h2 id="lea">Leadership & Experience</h2>
+        </div>
+
+        <div className="lea-mid">
+            <dl className="dl-grid">
+                <dt className="list-dl">
+                    <a id="list-gp-link" href = "https://www.globalpayments.com/" target = "_blank" >Global Payments DevOps Intern</a>
+                </dt> 
+                <dd className="list-dd">
+                    June 2024 - August 2024
+                </dd>
+    
+                <dt className="list-dl">
+                    UGA Game Builders Club Projects Officer
+                </dt>
+                <dd className="list-dd">
+                    December 2023 - Present
+                </dd>
+            </dl>
+        </div>
+
+        <div className="lea-bot">
+            <div id="erwin-quote-block">
+                <p className="quote">
+                    "Because my soldiers do not buckle or yield when faced with the cruelty of this world! My soldiers push forward! My soldiers scream out! My soldiers RAGE!"
+                </p>
+                <p className="quoter">- Erwin Smith</p>
+            </div>
+
+            <div id="ben-quote-block">
+                <p className="quote">
+                    "With great power comes great responsibility"
+                </p>
+                <p className="quoter">- Ben Parker</p>
+            </div>
+        </div>
+
+        <hr className="lea-line"></hr>
+        <br></br>  
+    </section>
+    ); 
+}
--- next-cv/src/app/page.tsx
+++ next-cv/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Nav from "./components/Nav";
 import Splash from "./components/Splash";
+import Experience from "./components/Experience";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <Nav/>
       <Splash/>
+      <Experience/>
     </div>
   );
 }
