2c618f63680d42afe1e2c5c9e749587497321153
Opuhlos <140357050+Opuhlos@users.noreply.github.com>
2024-10-22 23:30:31 -0400

    wip

--- next-cv/src/app/components/Experience.tsx
+++ next-cv/src/app/components/Experience.tsx
@@ -1,45 +1,52 @@
+import styles from './Experience.module.css'
+import {Archivo_Black, Noto_Sans, Prompt} from 'next/font/google'
+
+const notosans = Noto_Sans({subsets: ['latin']})
+const archivo = Archivo_Black({subsets: ['latin'], weight: ["400"]})
+const prompt = Prompt({subsets: ['latin'], weight: ["400", "300"]})
+
 export default function Experience() {
     return(
-        <section className="lea-section-container">
-        <div className="lea-top">
-            <h2 id="lea">Leadership & Experience</h2>
+        <section className={styles.leaSectionContainer}>
+        <div className={styles.leaTop}>
+            <h2 className={`${notosans.className} ${styles.lea}`}>Leadership & Experience</h2>
         </div>
 
-        <div className="lea-mid">
-            <dl className="dl-grid">
-                <dt className="list-dl">
-                    <a id="list-gp-link" href = "https://www.globalpayments.com/" target = "_blank" >Global Payments DevOps Intern</a>
+        <div className={styles.leaMid}>
+            <dl className={styles.dlGrid}>
+                <dt className={`${archivo.className} ${styles.listDL}`}>
+                    <a className={styles.listGpLink} href = "https://www.globalpayments.com/" target = "_blank" >Global Payments DevOps Intern</a>
                 </dt> 
-                <dd className="list-dd">
+                <dd className={`${prompt.className} ${styles.listDD}`}>
                     June 2024 - August 2024
                 </dd>
     
-                <dt className="list-dl">
+                <dt className={`${archivo.className} ${styles.listDL}`}>
                     UGA Game Builders Club Projects Officer
                 </dt>
-                <dd className="list-dd">
+                <dd className={`${prompt.className} ${styles.listDD}`}>
                     December 2023 - Present
                 </dd>
             </dl>
         </div>
 
-        <div className="lea-bot">
-            <div id="erwin-quote-block">
-                <p className="quote">
+        <div className={styles.leaBot}>
+            <div className={styles.erwinQuoteBlock}>
+                <p className={`${prompt.className} ${styles.quote}`}>
                     "Because my soldiers do not buckle or yield when faced with the cruelty of this world! My soldiers push forward! My soldiers scream out! My soldiers RAGE!"
                 </p>
-                <p className="quoter">- Erwin Smith</p>
+                <p className={`${archivo.className} ${styles.quoter}`}>- Erwin Smith</p>
             </div>
 
-            <div id="ben-quote-block">
-                <p className="quote">
+            <div className={styles.benQuoteBlock}>
+                <p className={`${prompt.className} ${styles.quote}`}>
                     "With great power comes great responsibility"
                 </p>
-                <p className="quoter">- Ben Parker</p>
+                <p className={`${archivo.className} ${styles.quoter}`}>- Ben Parker</p>
             </div>
         </div>
 
-        <hr className="lea-line"></hr>
+        <hr className={styles.leaLine}></hr>
         <br></br>  
     </section>
     ); 
