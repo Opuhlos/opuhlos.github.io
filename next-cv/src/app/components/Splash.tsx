3b2a06c77681a1ef80f418b604dc66c57ce3d796
opuhlos-eternity <ericnguyen455@gmail.com>
2024-10-23 15:09:49 -0400

    Education component

--- next-cv/src/app/components/Splash.tsx
+++ next-cv/src/app/components/Splash.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Education from './Education'
 import styles from './Splash.module.css'
 import {Archivo_Black, Noto_Sans, Prompt} from 'next/font/google'
 
@@ -10,27 +10,7 @@ const archivo = Archivo_Black({subsets: ['latin'], weight: ["400"]})
 export default function Splash() {
     return(
         <section className={styles.splash}>
-            <section className={styles.infoBar}>
-                <Image className={styles.profile} src="/images/headshot.jpg" alt="Headshot of Eric" width={250} height={250}></Image>
-                
-                <p className={`${prompt.className} ${styles.addressParagraph}`}>551 Starstream Street &#8226; Ohio, GA 300221</p>
-
-                <div className={styles.infoBox}>
-                    <h2 className={`${notosans.className} ${styles.h2}`}>Education</h2>
-                    <p className={`${prompt.className} ${styles.boxText}`}>
-                        University of Georgia | GPA 3.93
-                        <br></br>
-                        Franklin College of Arts & Sciences
-                        <br></br>
-                        Bachelors of Science in Computer Science
-                    </p>
-                </div>
-
-                <div className={styles.infoBox}>
-                    <h2 className={`${notosans.className} ${styles.h2}`}>Relevant Courses</h2>
-                    <p className={styles.boxText}>Introduction to Computing and Programming, Software Development, Systems Programming, Discrete Mathematics, Introduction to Theory of Computing, Data Structures</p>
-                </div>
-            </section>
+           <Education/>
 
             <div className={styles.tagContainer}>
                 <h1 className={`${notosans.className} ${styles.h1}`}>Welcome &#10022; Chào !</h1>
