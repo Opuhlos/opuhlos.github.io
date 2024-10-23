7a6f39dca6e63b073f6f238498895857237ed8ee
Opuhlos <140357050+Opuhlos@users.noreply.github.com>
2024-10-22 22:12:27 -0400

    Splash

--- next-cv/src/app/components/Splash.tsx
+++ next-cv/src/app/components/Splash.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image"
 import styles from './Splash.module.css'
-import {Noto_Sans, Prompt} from 'next/font/google'
+import {Archivo_Black, Noto_Sans, Prompt} from 'next/font/google'
 
 const notosans = Noto_Sans({subsets: ['latin']})
 const prompt = Prompt({subsets: ['latin'], weight: ["400", "300"]})
+const archivo = Archivo_Black({subsets: ['latin'], weight: ["400"]})
 
 
 export default function Splash() {
@@ -30,6 +31,26 @@ export default function Splash() {
                     <p className={styles.boxText}>Introduction to Computing and Programming, Software Development, Systems Programming, Discrete Mathematics, Introduction to Theory of Computing, Data Structures</p>
                 </div>
             </section>
+
+            <div className={styles.tagContainer}>
+                <h1 className={`${notosans.className} ${styles.h1}`}>Welcome &#10022; Chào !</h1>
+                <div className={styles.centerElement}>
+                    <p className={`${archivo.className} ${styles.tagLine}`}>
+                        Hello, I'm Eric Nguyen, a 3rd year Computer Science student, 2D/3D artist,
+                        and hobbyist game developer!
+                    </p>
+
+                </div>
+
+                <div className={styles.palette}>
+                    <div className={styles.block1}></div>
+                    <div className={styles.block2}></div>
+                    <div className={styles.block3}></div>
+                    <div className={styles.block4}></div>
+                    <div className={styles.block5}></div>
+                </div>
+            </div>
+
         </section>
     );
 }
